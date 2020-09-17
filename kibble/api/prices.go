c10f1a77e99c0e1a431a2a945496ac54399785ab
Mark Sargent <99003+sarge@users.noreply.github.com>
2020-09-17 16:26:19 +1200

    use DefaultPricingCountryCode. Track number of prices loaded

--- kibble/api/prices.go
+++ kibble/api/prices.go
@@ -34,7 +34,7 @@ func LoadAllPrices(cfg *models.Config, site *models.Site, itemIndex models.ItemI
 	sort.Strings(slugs)
 
 	const batchSize = 300
-
+	var total = 0
 	var s []string
 	for len(slugs) > 0 {
 
@@ -46,23 +46,27 @@ func LoadAllPrices(cfg *models.Config, site *models.Site, itemIndex models.ItemI
 			slugs = nil
 		}
 
-		err := loadPrices(cfg, site, s, itemIndex)
+		count, err := loadPrices(cfg, site, s, itemIndex)
 		if err != nil {
 			return err
 		}
+		total += count
 	}
+
+	log.Infof("prices: loaded %d for %s", total, cfg.DefaultPricingCountryCode)
 
 	return nil
 }
 
-func loadPrices(cfg *models.Config, site *models.Site, slugs []string, itemIndex models.ItemIndex) error {
+func loadPrices(cfg *models.Config, site *models.Site, slugs []string, itemIndex models.ItemIndex) (int, error) {
 
 	ids := strings.Join(slugs, ",")
-	path := fmt.Sprintf("%s/services/pricing/v2/prices/show_multiple?items=%s", cfg.SiteURL, ids)
+	path := fmt.Sprintf("%s/services/pricing/v2/prices/show_multiple?items=%s&location=%s", cfg.SiteURL, ids, cfg.DefaultPricingCountryCode)
 
 	data, err := Get(cfg, path)
 	if err != nil {
-		return err
+		log.Infof("pricing failed to load %s", err)
+		return 0, err
 	}
 
 	var details prices
@@ -70,14 +74,15 @@ func loadPrices(cfg *models.Config, site *models.Site, slugs []string, itemIndex
 	if err != nil {
 		log.Error("price.error: %s", err)
 		log.Debug("invalid data %s", string(data))
-		return err
+		return 0, err
 	}
 
 	return processPrices(details, site, itemIndex)
 }
 
-func processPrices(details prices, site *models.Site, itemIndex models.ItemIndex) error {
+func processPrices(details prices, site *models.Site, itemIndex models.ItemIndex) (int, error) {
 
+	count := 0
 	for _, p := range details.Prices {
 		found := itemIndex.Get(p.Item)
 
@@ -86,12 +91,14 @@ func processPrices(details prices, site *models.Site, itemIndex models.ItemIndex
 		switch found.ItemType {
 		case "film":
 			if f, ok := site.Films.FindFilmBySlug(p.Item); ok {
+				count++
 				f.Prices = pricingInfo
 				// replace the itemIndex
 				itemIndex.Replace(p.Item, f.GetGenericItem())
 			}
 		case "tvseason":
 			if f, ok := site.TVSeasons.FindTVSeasonBySlug(p.Item); ok {
+				count++
 				f.Prices = pricingInfo
 				// replace the itemIndex
 				itemIndex.Replace(p.Item, f.GetGenericItem())
@@ -99,6 +106,7 @@ func processPrices(details prices, site *models.Site, itemIndex models.ItemIndex
 
 		case "bundle":
 			if f, err := site.Bundles.FindBundleBySlug(p.Item); err == nil {
+				count++
 				f.Prices = pricingInfo
 				// replace the itemIndex
 				itemIndex.Replace(p.Item, f.GetGenericItem())
@@ -106,7 +114,7 @@ func processPrices(details prices, site *models.Site, itemIndex models.ItemIndex
 		}
 	}
 
-	return nil
+	return count, nil
 }
 
 type prices struct {
