c2acce1801bc60f97c47ef5d590f80f4b8a9bcaf
stajs <shannonsnz@gmail.com>
2021-11-04 12:52:02 +1300

    Clearer method names

--- kibble/api/languages.go
+++ kibble/api/languages.go
@@ -25,15 +25,14 @@ import (
 // Loads all languages from the v1 languages API for the given site if it has the translations_api feature toggle enabled.
 func LoadAllLanguages(cfg *models.Config, site *models.Site) error {
 	if site.Toggles["translations_api"] {
-		return loadFromApi(cfg, site)
+		return loadAllLanguagesFromApi(cfg, site)
 	} else {
-		loadFromConfig(cfg, site)
+		loadAllLanguagesFromConfig(cfg, site)
 		return nil
 	}
 }
 
-func loadFromApi(cfg *models.Config, site *models.Site) error {
-
+func loadAllLanguagesFromApi(cfg *models.Config, site *models.Site) error {
 	path := fmt.Sprintf("%s/services/users/v1/languages", cfg.SiteURL)
 
 	data, err := Get(cfg, path)
@@ -95,7 +94,7 @@ func formatPathLocale(code string) string {
 	return strings.ToLower(dashedCode)
 }
 
-func loadFromConfig(cfg *models.Config, site *models.Site) {
+func loadAllLanguagesFromConfig(cfg *models.Config, site *models.Site) {
 	var keys []string
 	for k := range cfg.Languages {
 		keys = append(keys, k)
